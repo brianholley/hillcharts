399edca6f9d4a4af22db80d54377596963e9a864
Brian Holley <brian.holley@hotmail.com>
2018-06-20 23:26:43 -0700

    Add starting support for webpack dev server hot reloading

---
+++ webpack.dev.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    target: 'web',
+    mode: 'development',
+    entry: './scripts/chart.js',
+    output: {
+        filename: 'scripts/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style', 'css', 'sass']
+            }
+        ]
+    },
+    devServer: {
+        https: true
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './*.html', to: './' },
+            { from: './css', to: 'css' },
+            { from: './libs', to: 'libs' },
+            { from: './img', to: 'img' },
+            { from: './vss-extension.json', to: 'vss-extension.json' }
+        ])
+    ]
+}
